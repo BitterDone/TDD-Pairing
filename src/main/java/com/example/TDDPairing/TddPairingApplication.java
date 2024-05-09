5ad211a096390544007d5da6c66793dcb8f5c4c4
Dan Bitter <bitter.daniel@gmail.com>
2024-05-09 13:26:16 -0400

    Initial commit of Spring initializer project

---
+++ src/main/java/com/example/TDDPairing/TddPairingApplication.java
@@ -0,0 +1,13 @@
+package com.example.TDDPairing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddPairingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddPairingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TDDPairing/TddPairingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TDDPairing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddPairingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
