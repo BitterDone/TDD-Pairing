e211a9c71065a60bf3770d3cfb3d3b531371ff7b
Dan Bitter <bitter.daniel@gmail.com>
2024-05-14 17:39:22 -0400

    001 - Add unit test for display values from the backend

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,7 +1,30 @@
+import { useState, useEffect } from 'react'
+
 import logo from '../../logo.svg'
 import './App.css'
 
 function App () {
+  const [displayValue, setDisplayValue] = useState('loading')
+  const [counter, setCounter] = useState(0)
+
+  function doFetch () {
+    fetch('http://localhost:8080/hello')
+      .then(res => {
+        return res.text()
+      })
+      .then(data => {
+        setDisplayValue(data)
+        setCounter(counter + 1)
+      })
+      .catch(e => {
+        setDisplayValue('Error loading data')
+      })
+  }
+
+  useEffect(() => {
+    doFetch()
+  }, [])
+
   return (
     <div className='App'>
       <header className='App-header'>
@@ -9,14 +32,20 @@ function App () {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Displaying: {displayValue}!
+        </p>
         <a
           className='App-link'
           href='https://reactjs.org'
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React
+          Learn React {counter}
         </a>
+        <button onClick={() => doFetch()}>
+          Load data
+        </button>
       </header>
     </div>
   )
--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -1,16 +1,33 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, waitFor } from '@testing-library/react'
+import App from './App'
+import userEvent from '@testing-library/user-event'
+import { act } from 'react'
 
-describe('test-name', () => {
-  test('renders learn react link', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-  });
-  
-  test('renders data from the backend', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/hello/i);
-    expect(linkElement).toBeInTheDocument();
-  });
-});
+describe('App component', () => {
+  describe('Static text', () => {
+    test('renders learn react link', () => {
+      render(<App />)
+      const linkElement = screen.getByText(/learn react/i)
+      expect(linkElement).toBeInTheDocument()
+    })
+  })
+
+  describe('Network data', () => {
+    test('renders data from the backend', async () => {
+      jest.spyOn(global, 'fetch').mockResolvedValue({ text: () => 'hello' })
+
+      render(<App />)
+      await waitFor(() => {
+        expect(screen.getByText(/Displaying: hello/i)).toBeInTheDocument()
+      })
+      expect(screen.getByText(/Learn React 1/i)).toBeInTheDocument()
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+
+      await act(async () => await userEvent.click(screen.getByText(/Load data/i)))
+
+      expect(global.fetch).toHaveBeenCalledTimes(2)
+      expect(screen.getByText(/Learn React 2/i)).toBeInTheDocument()
+      expect(screen.getByText(/Displaying: hello/i)).toBeInTheDocument()
+    })
+  })
+})
