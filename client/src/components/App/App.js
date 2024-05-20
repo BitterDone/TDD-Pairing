32d3b8dded01073926ce656e10fc37b33170f7bc
Dan Bitter <bitter.daniel@gmail.com>
2024-05-19 22:55:02 -0400

    001 - Update App to use useFetch

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,29 +1,39 @@
 import { useState, useEffect } from 'react'
+import useFetch from '../utilities/network'
 
 import logo from '../../logo.svg'
 import './App.css'
+import ChildComponent from '../ChildComponent/ChildComponent'
 
 function App () {
   const [displayValue, setDisplayValue] = useState('loading')
   const [counter, setCounter] = useState(0)
 
-  function doFetch () {
-    fetch('http://localhost:8080/hello')
-      .then(res => {
-        return res.text()
-      })
-      .then(data => {
-        setDisplayValue(data)
-        setCounter(counter + 1)
-      })
-      .catch(e => {
-        setDisplayValue('Error loading data')
-      })
-  }
+  const url = 'http://localhost:8080/hello'
+  const { doFetch, loading, result, error } = useFetch(url)
 
   useEffect(() => {
-    doFetch()
+    doFetch(url)
   }, [])
+
+  useEffect(() => {
+  if (loading === true) {
+    setDisplayValue('loading')
+    return
+  }
+  
+  if (result !== undefined) {
+    setDisplayValue(result)
+    setCounter(counter + 1)
+    return
+  }
+  
+  if (error !== undefined) {
+    setDisplayValue('Error loading data' + error)
+    return
+  }
+
+  }, [loading, result, error])
 
   return (
     <div className='App'>
@@ -43,8 +53,8 @@ function App () {
         >
           Learn React {counter}
         </a>
-        <button onClick={() => doFetch()}>
-          Load data
+        <button onClick={() => doFetch(url)}>
+          <ChildComponent message='Load data' />
         </button>
       </header>
     </div>
